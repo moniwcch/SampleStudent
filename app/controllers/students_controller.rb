99c4ade47fbd74e6f3ce6b597c2ef488cb42305b
moniwcch <mohineesh.wcch@gmail.com>
2020-04-14 00:14:34 +0530

    (CRUD)

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,41 @@
+class StudentsController < ApplicationController
+before_action:set_articles, only: [:show,:destroy,:edit,:update]
+    def show
+        
+    end
+    def index
+        @student=Student.all
+    end
+    def new
+        @student=Student.new
+    end
+    def create
+        @student=Student.new(set_params)
+        # render plain: student.inspect
+        if @student.save
+            redirect_to @student
+        else
+        render 'new'
+        end
+    end
+    def edit
+        
+    end
+    def update
+    
+    student=@student.update(set_params)
+    redirect_to @student
+      end
+     def destroy
+       
+        @student.destroy
+        redirect_to students_path
+     end
+     private
+     def set_articles
+        @student=Student.find(params[:id])
+     end
+     def set_params
+        params.require(:student).permit(:firstname, :roll)
+    end
+end 
